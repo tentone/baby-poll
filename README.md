40a7423fdbbd3d3ec33b736648c7b3d9bb17f366
Tentone <tentone@outlook.com>
2024-06-09 16:39:58 +0100

    App

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# baby-poll
-Simple app for a gender reveal poll
+# Baby Poll
+ - Simple app for a gender reveal poll
+ - Composed of client and server components
+ - The client is a vue.js app and the server is built with Node.js and Express
+
+## Usage
+ - Install [node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/)
+ - Clone the repository
+ - Run `npm install` in the project directory
+
+## License
+ - The project is distributed under [MIT](https://opensource.org/licenses/MIT), available in the LICENSE file
---
+++ server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import * as fs from 'fs';
+
+/**
+ * Main application class.
+ */
+class Server {
+    constructor(config) {
+        this.config = config;
+
+        this.loadData();
+    }
+
+    /**
+     * Load data from the database.
+     */
+    loadData() {
+        if (fs.existsSync(this.config.database)) {
+            console.log(' - Database file exists, reading data.');
+            this.data = JSON.parse(fs.readFileSync(this.config.database));
+        } else {
+            console.log(' - Database file does not exist, creating new data.');
+            this.data = {
+                results: {
+                    male: 0,
+                    female: 0
+                },
+                votes: []
+            };
+            this.saveData();
+        }
+    }
+
+    /**
+     * Save data to the database.
+     */
+    saveData() {
+        console.log(' - Saving data to the database.')
+        fs.writeFileSync(this.config.database, JSON.stringify(this.data));
+    }
+
+    /**
+     * Start the server.
+     */
+    start() {
+        console.log(' - Starting server with config ', this.config);
+
+        this.app = express();
+
+        this.app.get('/results', (req, res) => {
+            res.send(this.data.results);
+        });
+        
+        this.app.post('/vote', (req, res) => {
+            console.log(req);
+
+            console.log(' - Received a vote.');
+            // this.data.results[req.query] += 1;
+
+        });
+        
+        this.app.listen(this.config.port, () => {
+            console.log(` - Server running at http://localhost:${this.config.port}`);
+        });
+    }
+}
+
+
+new Server({
+    port: 3000,
+    database: 'database.json'
+}).start();
